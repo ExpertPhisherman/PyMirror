ef68ca33ce4c79cd73a0c613b9bcb43f53cb34e1
Logan Tischler <35463649+logantischler0@users.noreply.github.com>
2022-03-10 10:20:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
 
-# Convert list items to float
-def floatList(myList):
-    return [float(i) for i in myList]
+# Initialize variables
+x = sp.Symbol('x', real = True)
+mir = x + 1
+obj = x ** 2
+xBounds = (-5, 5)
+xStep = 0.1
+decPlaces = 4
 
 # Flatten list
 def flatten(myList):
@@ -14,13 +18,9 @@ def flatten(myList):
     except:
         return myList
 
-# Initialize variables
-x = sp.Symbol('x')
-mir = 2 * x + 1
-obj = x - 1
-xBounds = (-5, 5)
-xStep = 0.1
-decPlaces = 4
+# Convert list items to float
+def floatList(myList):
+    return [float(i) for i in myList]
 
 # Approximate function zeros using Householder's method
 def householderZeros(func, var, guess, order, epochs):
@@ -42,18 +42,46 @@ def norm(func, var, pt):
 def reflect(pt, center):
     return 2 * center - pt
 
-ptsMir = np.arange(xBounds[0], xBounds[1] + xStep, xStep)
-ptsObj = [sp.solve(norm(mir, x, ptMir) - obj) for ptMir in ptsMir]
-ptsRefl = [reflect(ptObj[0], ptMir) for ptObj, ptMir in zip(ptsObj, ptsMir)]
+mir = sp.sympify(mir)
+obj = sp.sympify(obj)
 
-ptsMir = floatList(flatten(ptsMir))
-ptsObj = floatList(flatten(ptsObj))
-ptsRefl = floatList(flatten(ptsRefl))
+ptsMir_x = np.arange(xBounds[0], xBounds[1] + xStep, xStep)
+print([norm(mir, x, ptMir_x) for ptMir_x in ptsMir_x])
+ptsObj_x = [sp.solve(norm(mir, x, ptMir_x) - obj) for ptMir_x in ptsMir_x]
+ptsRefl_x = [reflect(ptObj_x, ptMir_x) for ptObj_x, ptMir_x in zip(ptsObj_x, ptsMir_x)]
 
-print('ptsObj:', ptsObj, '\n')
-print('ptsMir:', ptsMir, '\n')
-print('ptsRefl:', ptsRefl, '\n')
+ptsMir_x = floatList(flatten(ptsMir_x))
+ptsObj_x = floatList(flatten(ptsObj_x))
+ptsRefl_x = floatList(flatten(ptsRefl_x))
 
-'''print(len(ptsObj), '\n')
-print(len(ptsMir), '\n')
-print(len(ptsRefl), '\n')'''
+ptsMir_y = [mir.subs(x, i) for i in ptsMir_x]
+ptsObj_y = [obj.subs(x, i) for i in ptsObj_x]
+ptsRefl_y = [reflect(ptObj_y, ptMir_y) for ptObj_y, ptMir_y in zip(ptsObj_y, ptsMir_y)]
+
+ptsMir_y = floatList(flatten(ptsMir_y))
+ptsObj_y = floatList(flatten(ptsObj_y))
+ptsRefl_y = floatList(flatten(ptsRefl_y))
+
+ptsRefl_x = ptsRefl_x[:len(ptsRefl_y)]
+
+print('ptsObj_x:', ptsObj_x, '\n')
+print('ptsMir_x:', ptsMir_x, '\n')
+print('ptsRefl_x:', ptsRefl_x, '\n')
+
+print(len(ptsObj_x), '\n')
+print(len(ptsMir_x), '\n')
+print(len(ptsRefl_x), '\n')
+
+print('ptsObj_y:', ptsObj_y, '\n')
+print('ptsMir_y:', ptsMir_y, '\n')
+print('ptsRefl_y:', ptsRefl_y, '\n')
+
+print(len(ptsObj_y), '\n')
+print(len(ptsMir_y), '\n')
+print(len(ptsRefl_y), '\n')
+
+plt.plot(ptsMir_x, ptsMir_y, label = 'Mirror')
+plt.plot(ptsObj_x, ptsObj_y, label = 'Object')
+plt.plot(ptsRefl_x, ptsRefl_y, label = 'Reflection')
+plt.legend()
+plt.show()
